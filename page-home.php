f885c619f2fcaa91c0c5c78882dc6fdf321c5ea8
ansolvera <38958444+ansolvera@users.noreply.github.com>
2022-05-02 13:18:48 -0700

    Add login info

--- page-home.php
+++ page-home.php
@@ -23,6 +23,18 @@
 
 $context = Timber::context();
 
+$args = array(
+    'post_type' => 'optin',
+    'post_status' => 'publish',
+    'perm' => 'readable',
+    'nopaging' => true,
+    'meta_key' => 'weight',
+    'orderby' => 'meta_value_num',
+    'order' => 'ASC',
+);
+
+$context['optins'] =  new Timber\PostQuery($args);
+
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
 Timber::render( array( 'pages/' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
